68704a015e20bd3d6275ed639cc05cd1d32ffc5a
Léo Flaventin Hauchecorne <hl037.prog@gmail.com>
2021-09-13 14:54:27 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div id="d1">
+      <svg>
+        <use href="@/assets/logo.svg#logo"></use>
+      </svg>
+    </div>
+    <div id="d2">
+      <svg>
+        <use href="@/assets/logo.svg#logo"></use>
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+});
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+svg {
+  height: 100px;
+}
+</style>
